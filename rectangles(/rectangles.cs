317a518c07ae0eb7f895353ee13765a9d5595ae7
CosimoTatini <151754636+CosimoTatini@users.noreply.github.com>
2024-01-30 10:17:02 +0100

    Update rectangles.cs

--- rectangles(/rectangles.cs
+++ rectangles(/rectangles.cs
@@ -1,8 +1,7 @@
-//Implementare un programma che gestisce un insieme di figure composte da rettangoli. 
-//Creare classi per i rettangoli e le figure composte, con metodi per aggiungere, rimuovere, modificare rettangoli e calcolare l'area e il perimetro delle figure composte.
 
 
-using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
 
 public class Rettangolo
 {
@@ -10,10 +9,10 @@ public class Rettangolo
     public double Altezza { get; set; }
     public Rettangolo(double Lunghezza, double Altezza)
     {
-        Lunghezza = Lunghezza;
-        Altezza = Altezza;
+        this.Lunghezza = Lunghezza;
+        this.Altezza = Altezza;
     }
-    public double Area(double lunghezza, double Altezza)
+    public double CalcolaArea()
     {
 
         return Lunghezza * Altezza;
@@ -40,48 +39,96 @@ public class Composta
     }
     public void RemoveRekt(Rettangolo rettangolo)
     {
-      for(int i = 0; i< rettangolos.Count; i++) 
-      {
-            if (rettangolo.Lunghezza == rettangolo.Altezza && rettangolos[i].Lunghezza ==rettangolos[i].Altezza )
+        for (int i = 0; i < rettangolos.Count; i++)
+        {
+            if (rettangolo.Lunghezza == rettangolo.Altezza && rettangolos[i].Lunghezza == rettangolos[i].Altezza)
             {
 
                 rettangolos.RemoveAt(i);
 
             }
-         
-        
-      }
+
+
+        }
 
     }
-    public void ModifyRektMeas(Rettangolo rettangolo, double newaltezza, int newlunghezza)
+    public void ModifyRektMeas(Rettangolo rettangolo, double newaltezza, double newlunghezza)
     {
-      
-     for (int i = 0;i< rettangolos.Count;i++) 
-     { 
-        
-       if (rettangolos[i].Lunghezza==rettangolo.Lunghezza && rettangolos[i].Altezza == rettangolo.Altezza) 
-       {
+
+        for (int i = 0; i < rettangolos.Count; i++)
+        {
+
+            if (rettangolos[i].Lunghezza == rettangolo.Lunghezza && rettangolos[i].Altezza == rettangolo.Altezza)
+            {
 
                 rettangolos[i].Altezza = newaltezza;
-                rettangolos[i].Lunghezza= newlunghezza;
-            
-       } 
-       
-        
-     }
+                rettangolos[i].Lunghezza = newlunghezza;
+
+            }
+
+
+        }
 
 
     }
-    public double CalculateAreaTotale() 
+    public double CalculateAreaTotale()
     {
-      double somma = 0;
-     foreach( Rettangolo rekt in rettangolos ) 
-     {
+        double sommaA = 0;
+        foreach (Rettangolo rekt in rettangolos)
+        {
 
-      rekt.Area();
-      somma += rekt.Area();  
-        
-     }
-     return somma;
+            rekt.CalcolaArea();
+            sommaA += rekt.CalcolaArea();
+
+        }
+        return sommaA;
+    }
+    public double CalculateTotPerimeter()
+    
+    { 
+     double sommaP=0;
+        foreach (Rettangolo rekt in rettangolos)
+        {
+
+            rekt.Perimetro();
+            sommaP += rekt.Perimetro();
+
+
+        }
+        return sommaP;
+    
+    
+    
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Rettangolo rettangolo1 = new Rettangolo(10, 20);
+        Rettangolo rettangolo2 = new Rettangolo(40, 50);
+        Composta composta = new Composta();
+        composta.AddRekt(rettangolo1);
+        composta.AddRekt(rettangolo2);
+        double Areatot = composta.CalculateAreaTotale();
+        Console.WriteLine($"L'area totale della figura è : {Areatot}");
+        double Totperimeter = composta.CalculateTotPerimeter();
+        Console.WriteLine($"Il perimetro totale della figura è :{Totperimeter}");
+        composta.RemoveRekt(rettangolo1);
+
+        Rettangolo rettangolo3 = new Rettangolo(50, 60);
+        composta.ModifyRektMeas(rettangolo2, rettangolo3.Altezza, rettangolo3.Lunghezza);
+
+        Areatot = composta.CalculateAreaTotale();
+        Console.WriteLine($"La nuova area dopo le modifiche è:{Areatot}");
+
+        Totperimeter = composta.CalculateTotPerimeter();
+        Console.WriteLine($"Il nuovo perimetro dopo le modifiche è:{Totperimeter}");
+
+
+
+
+
+
     }
 }
